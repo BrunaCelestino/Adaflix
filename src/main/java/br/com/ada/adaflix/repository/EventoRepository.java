46c76d6d14290b3d509077462e7ee2cc4f379b86
BrunaMassuchini <brunaccelestino@gmail.com>
2022-12-19 22:24:24 -0300

    feat: started application

---
+++ src/main/java/br/com/ada/adaflix/repository/EventoRepository.java
@@ -0,0 +1,6 @@
+package br.com.ada.adaflix.repository;
+import br.com.ada.adaflix.model.Evento;
+import org.springframework.data.repository.CrudRepository;
+
+public interface EventoRepository extends CrudRepository<Evento, Long> {
+}
